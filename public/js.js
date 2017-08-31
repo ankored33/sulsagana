6114bc44164dfabef9b7417d950933b531e4f956
Masato Doi <bridgehead33@gmail.com>
2017-08-31 13:16:30 +0000

    動き修正

--- public/js.js
+++ public/js.js
@@ -1,16 +1,16 @@
 $(function (){
     $('#main').on('click', '#button1', function(){
-        $('#cutin1').css({'margin-top':'-360px','margin-right':'490px'});
-        $('#cutin2').css({'margin-top':'360px','margin-right':'0px'});
+        $('#cutin1').css({'top':'-360px','left':'0px'});
+        $('#cutin2').css({'top':'360px','left':'490px'});
         $.when(
-            $('#cutin1').animate({'margin-top':'0px'}, 100),
-            $('#cutin2').animate({'margin-top':'0px'}, 100)
+            $('#cutin1').animate({'top':'0px'}, 100),
+            $('#cutin2').animate({'top':'0px'}, 100)
         ).done(function(){
                 setTimeout(function(){
-                    $('#cutin1').animate({'margin-right':'-10px'}, 100);
-                    $('#cutin1').animate({'margin-right':'0px'}, 30);
-                    $('#cutin2').animate({'margin-right':'500px'}, 100);
-                    $('#cutin2').animate({'margin-right':'490px'}, 30);
+                    $('#cutin1').animate({'left':'500px'}, 100);
+                    $('#cutin1').animate({'left':'490px'}, 30);
+                    $('#cutin2').animate({'left':'-10px'}, 100);
+                    $('#cutin2').animate({'left':'0px'}, 30);
                 },140);
         });
     });
@@ -19,17 +19,17 @@ $(function (){
 
 $(function (){
     $('#main').on('click', '#button2', function(){
-        $('#cutin3').css({'margin-top':'-0px','margin-right':'640px'});
-        $('#cutin4').css({'margin-top':'360px','margin-right':'0px'});
+        $('#cutin3').css({'top':'0px','left':'-640px'});
+        $('#cutin4').css({'top':'360px','left':'490px'});
         $.when(
-            $('#cutin3').animate({'margin-right':'0px'}, 100),
-            $('#cutin4').animate({'margin-top':'0px'}, 100)
+            $('#cutin3').animate({'left':'0px'}, 100),
+            $('#cutin4').animate({'top':'0px'}, 100)
         ).done(function(){
                 setTimeout(function(){
-                    $('#cutin3').animate({'margin-top':'250px'}, 100);
-                    $('#cutin3').animate({'margin-top':'240px'}, 30);
-                    $('#cutin4').animate({'margin-right':'500px'}, 100);
-                    $('#cutin4').animate({'margin-right':'490px'}, 30);
+                    $('#cutin3').animate({'top':'250px'}, 100);
+                    $('#cutin3').animate({'top':'240px'}, 30);
+                    $('#cutin4').animate({'left':'-10px'}, 100);
+                    $('#cutin4').animate({'left':'0px'}, 30);
                 },140);
         });
     });

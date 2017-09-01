603dac618984d5571873c97e0c161df048080cda
Masato Doi <bridgehead33@gmail.com>
2017-09-01 06:40:48 +0000

    一旦動きは完成で

--- public/js.js
+++ public/js.js
@@ -1,8 +1,11 @@
 $(function (){
     $('#main').on('click', '#button1', function(){
+        var radioVal = $('input[name="type"]:checked').val();
+        console.log(radioVal);
+
         erase_screenshot();
-        $('#cutin1').css({'top':'-360px','left':'0px'});
-        $('#cutin2').css({'top':'360px','left':'490px'});
+        positionReset();
+        $('.cutin').css('display','table');
         var redTxt = $('#cutin2 > span:nth-child(1)').text();
         var sub = redTxt.substr(redTxt.length-2,1);
         $('#cutin2 > span:nth-child(1)').html(redTxt.replace(sub,'<span style="color:red">' + sub + '</span>'));
@@ -24,9 +27,7 @@ $(function (){
 
 $(function (){
     $('#main').on('click', '#button2', function(){
-        $('#cutin3').css('text-align','left');
-        $('#cutin3').css({'top':'0px','left':'-640px'});
-        $('#cutin4').css({'top':'360px','left':'490px'});
+        positionReset();
         var redTxt = $('#cutin4 > span:nth-child(1)').text();
         var sub = redTxt.substr(redTxt.length-2,1);
         $('#cutin4 > span:nth-child(1)').html(redTxt.replace(sub,'<span style="color:red">' + sub + '</span>'));
@@ -60,4 +61,22 @@ function erase_screenshot() {
     $('#screen_image')[0].src = "";
     $('#download')[0].href = "#";
     $('#download')[0].innerHTML = "";
+}
+
+function positionReset(){
+        $('#cutin1').css({'top':'-360px','left':'0px'});
+        $('#cutin2').css({'top':'360px','left':'490px'});
+        $('#cutin3').css({'top':'0px','left':'-640px','text-align':'left'});
+        $('#cutin4').css({'top':'360px','left':'490px'});        
+}
+
+function delCookie(){
+  //日付データを作成する
+  var date1 = new Date();
+  //1970年1月1日00:00:00の日付データをセットする
+  date1.setTime(0);
+  //有効期限を過去にして書き込む
+  document.cookie = "counts=;expires="+date1.toGMTString();
+  //ページを再読み込みする
+  location.reload();
 }

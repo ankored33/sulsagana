bf5afe1c73f096309e9dc6628fbdedffc0217dc1
Masato Doi <bridgehead33@gmail.com>
2017-09-01 00:40:49 +0000

    文字色変更

--- public/js.js
+++ public/js.js
@@ -2,6 +2,9 @@ $(function (){
     $('#main').on('click', '#button1', function(){
         $('#cutin1').css({'top':'-360px','left':'0px'});
         $('#cutin2').css({'top':'360px','left':'490px'});
+        var redTxt = $('#cutin2 > span:nth-child(1)').text();
+        var sub = redTxt.substr(redTxt.length-2,1);
+        $('#cutin2 > span:nth-child(1)').html(redTxt.replace(sub,'<span style="color:red">' + sub + '</span>'));
         $.when(
             $('#cutin1').animate({'top':'0px'}, 100),
             $('#cutin2').animate({'top':'0px'}, 100)
@@ -22,6 +25,9 @@ $(function (){
         $('#cutin3').css('text-align','left');
         $('#cutin3').css({'top':'0px','left':'-640px'});
         $('#cutin4').css({'top':'360px','left':'490px'});
+        var redTxt = $('#cutin4 > span:nth-child(1)').text();
+        var sub = redTxt.substr(redTxt.length-2,1);
+        $('#cutin4 > span:nth-child(1)').html(redTxt.replace(sub,'<span style="color:red">' + sub + '</span>'));
         $.when(
             $('#cutin3').animate({'left':'0px'}, 100),
             $('#cutin4').animate({'top':'0px'}, 100)
